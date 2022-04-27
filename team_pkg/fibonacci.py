d0239422ddc6cd81fb13b75b7799ea847605e334
uytgb159 <rnalscjf00@naver.com>
2022-04-27 23:57:27 +0900

    line modified

--- team_pkg/fibonacci.py
+++ team_pkg/fibonacci.py
@@ -6,11 +6,11 @@ def fibo():
         print("wrong number")
         return
     elif(num==1):
-        print(1)
+        print(1, end=' ')
         print("F1 = 1")
     else:
         f=[0,1]
-        print(f[1])
+        print(f[1], end=' ')
         for i in range(2, num+1, 1):
             f.append(f[i-1]+f[i-2])
             print(f[i],end=' ')
